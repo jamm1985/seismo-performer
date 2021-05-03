d43277f4802938863df75ef1e6ce554dfce70e98
Andrey Stepnov <a.stepnov@geophystech.ru>
2021-05-03 11:11:01 +1100

    Add performer from google-research repo, add model with fast attention

--- seismo_transformer.py
+++ seismo_transformer.py
@@ -17,6 +17,11 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from einops.layers.tensorflow import Rearrange
 
+from fast_attention import fast_attention
+
+from kapre import STFT, Magnitude, MagnitudeToDecibel
+from kapre.composed import get_melspectrogram_layer, get_log_frequency_spectrogram_layer
+
 import numpy as np
 
 
@@ -72,7 +77,7 @@ def load_test_train_data(hdf5_file, proportion, random_state=1):
 
 
 """
-# Learnable classification token
+Learnable classification token
 """
 
 
@@ -96,7 +101,7 @@ class ClsToken(keras.layers.Layer):
 
 
 """
-# Learnable position embedding
+Learnable position embedding
 """
 
 
@@ -114,7 +119,9 @@ class PosEmbeding(keras.layers.Layer):
         return inputs + self.w
 
 
-# Rearrange 3 chanles with patches to 1 channel
+"""
+Rearrange 3 channels with patches to 1 channel
+"""
 class RearrangeCh(keras.layers.Layer):
     def __init__(self, num_patches=20, embed_dim=20):
         super(RearrangeCh, self).__init__()
@@ -125,17 +132,29 @@ class RearrangeCh(keras.layers.Layer):
 
 
 """
-Title: Text classification with Transformer
-Author: [Apoorv Nandan](https://twitter.com/NandanApoorv)
-## Implement a Transformer block as a layer
+Rearrange 3d channels 
 """
+class Rearrange3d(keras.layers.Layer):
+    def __init__(self, p1, p2):
+        super(Rearrange3d, self).__init__()
+        self.rearrange = Rearrange('b (h p1) (w p2) c -> b (h w) (p1 p2 c)',
+                p1 = p1, p2 = p2)
+
+    def call(self, inputs):
+        return self.rearrange(inputs)
 
 
+"""
+Implement a Transformer block as a layer
+Credit:
+    Title: Text classification with Transformer
+    Author: [Apoorv Nandan](https://twitter.com/NandanApoorv)
+"""
 class TransformerBlock(layers.Layer):
     def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
         super(TransformerBlock, self).__init__()
-        self.att = tf.keras.layers.MultiHeadAttention(
-            num_heads=num_heads, key_dim=embed_dim, dropout=rate)
+        self.att = layers.MultiHeadAttention(
+            num_heads=num_heads, key_dim=embed_dim)
         self.ffn = keras.Sequential(
             [layers.Dense(ff_dim,
                 activation='gelu'),
@@ -155,6 +174,32 @@ class TransformerBlock(layers.Layer):
         return self.layernorm2(out1 + ffn_output)
 
 
+class PerformerBlock(layers.Layer):
+    def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
+        super(PerformerBlock, self).__init__()
+        self.att = fast_attention.Attention(
+            num_heads=num_heads, hidden_size=embed_dim, attention_dropout=0.0)
+        self.ffn = keras.Sequential(
+            [layers.Dense(ff_dim,
+                activation='gelu'),
+                layers.Dense(embed_dim), ]
+        )
+        self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = layers.Dropout(rate)
+        self.dropout2 = layers.Dropout(rate)
+
+    def call(self, inputs, training):
+        attn_output = self.att(inputs, inputs, bias=None)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(inputs + attn_output)
+        ffn_output = self.ffn(out1)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        return self.layernorm2(out1 + ffn_output)
+
+"""
+Models section
+""" 
 def seismo_transformer(
         maxlen=400,
         patch_size=25,
@@ -165,7 +210,7 @@ def seismo_transformer(
         layers_depth=8,
         num_classes=3,
         drop_out_rate=0.1):
-    """Create classifier model using ViT approach with transformer blocks
+    """Model for P/S/N waves classification using ViT approach
     :maxlen: maximum samples of waveforms
     :patch_size: patch size for every single channel
     :num_channels: number of channels (usually it's equal to 3)
@@ -205,3 +250,207 @@ def seismo_transformer(
     model = keras.Model(inputs=inputs, outputs=outputs)
     return model
 
+
+def seismo_transformer_with_spec(
+        maxlen=400,
+        patch_size_1=2,
+        patch_size_2=3,
+        num_channels=3,
+        num_patches = 40,
+        nfft=128,
+        d_model=96,
+        num_heads=8,
+        ff_dim_factor=4,
+        layers_depth=8,
+        num_classes=3,
+        drop_out_rate=0.1):
+    """The model for P/S/N waves classification using ViT approach
+    with converted raw to spectrogram input
+    :maxlen: maximum samples of waveforms
+    :patch_size_1: patch size for first dimention
+    :patch_size_2: patch size for second dimention
+    :num_channels: number of channels (usually it's equal to 3)
+    :num_patches: resulting number of patches (FIX manual setup!)
+    :nfft: number of FFTs in short-time Fourier transform
+    :d_model: Embedding size for each token
+    :num_heads: Number of attention heads
+    :ff_dim_factor: Hidden layer size in feed forward network inside transformer
+                    ff_dim = d_model * ff_dim_factor
+    :layers_depth: The number of transformer blocks
+    :num_classes: The number of classes to predict
+    :returns: Keras model object
+    """
+    #num_patches = (maxlen // patch_size)**2
+    num_patches = num_patches
+    ff_dim = d_model * ff_dim_factor
+    inputs = layers.Input(shape=(maxlen, num_channels))
+    # x = tf.keras.layers.Permute((2, 1))(inputs)
+    # do transform
+    x = STFT(n_fft=nfft,
+            window_name=None,
+            pad_end=False,
+            input_data_format='channels_last',
+            output_data_format='channels_last',)(inputs)
+    x = Magnitude()(x)
+    x = MagnitudeToDecibel()(x)
+    # patch the input channel
+    # x = tf.keras.layers.Reshape((num_channels, num_patches, patch_size))(x)
+    x = Rearrange3d(p1=patch_size_1,p2=patch_size_2)(x)
+    # embedding
+    x = tf.keras.layers.Dense(d_model)(x)
+    # cls token
+    x = ClsToken(d_model)(x)
+    # positional embeddings
+    x = PosEmbeding(num_patches=num_patches + 1, embed_dim=d_model)(x)
+    # encoder block
+    x = layers.Dropout(drop_out_rate)(x)
+    for i in range(layers_depth):
+        x = TransformerBlock(d_model, num_heads, ff_dim)(x)
+    # to MLP head
+    x = tf.keras.layers.Lambda(lambda x: x[:, 0])(x)
+    # MLP-head
+    #x = tf.keras.layers.LayerNormalization(epsilon=1e-6)(x)
+    x = layers.Dropout(drop_out_rate)(x)
+    x = tf.keras.layers.Dense(ff_dim, activation='gelu')(x)
+    x = layers.Dropout(drop_out_rate)(x)
+    outputs = layers.Dense(num_classes, activation='softmax')(x)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+def seismo_performer_with_spec(
+        maxlen=400,
+        patch_size_1=2,
+        patch_size_2=3,
+        num_channels=3,
+        num_patches = 40,
+        nfft=128,
+        d_model=96,
+        num_heads=8,
+        ff_dim_factor=4,
+        layers_depth=8,
+        num_classes=3,
+        drop_out_rate=0.1):
+    """The model for P/S/N waves classification using ViT approach
+    with converted raw to spectrogram input
+    :maxlen: maximum samples of waveforms
+    :patch_size_1: patch size for first dimention
+    :patch_size_2: patch size for second dimention
+    :num_channels: number of channels (usually it's equal to 3)
+    :num_patches: resulting number of patches (FIX manual setup!)
+    :nfft: number of FFTs in short-time Fourier transform
+    :d_model: Embedding size for each token
+    :num_heads: Number of attention heads
+    :ff_dim_factor: Hidden layer size in feed forward network inside transformer
+                    ff_dim = d_model * ff_dim_factor
+    :layers_depth: The number of transformer blocks
+    :num_classes: The number of classes to predict
+    :returns: Keras model object
+    """
+    #num_patches = (maxlen // patch_size)**2
+    num_patches = num_patches
+    ff_dim = d_model * ff_dim_factor
+    inputs = layers.Input(shape=(maxlen, num_channels))
+    # x = tf.keras.layers.Permute((2, 1))(inputs)
+    # do transform
+    x = STFT(n_fft=nfft,
+            window_name=None,
+            pad_end=False,
+            input_data_format='channels_last',
+            output_data_format='channels_last',)(inputs)
+    x = Magnitude()(x)
+    x = MagnitudeToDecibel()(x)
+    # patch the input channel
+    # x = tf.keras.layers.Reshape((num_channels, num_patches, patch_size))(x)
+    x = Rearrange3d(p1=patch_size_1,p2=patch_size_2)(x)
+    # embedding
+    x = tf.keras.layers.Dense(d_model)(x)
+    # cls token
+    x = ClsToken(d_model)(x)
+    # positional embeddings
+    x = PosEmbeding(num_patches=num_patches + 1, embed_dim=d_model)(x)
+    # encoder block
+    x = layers.Dropout(drop_out_rate)(x)
+    for i in range(layers_depth):
+        x = PerformerBlock(d_model, num_heads, ff_dim)(x)
+    # to MLP head
+    x = tf.keras.layers.Lambda(lambda x: x[:, 0])(x)
+    # MLP-head
+    #x = tf.keras.layers.LayerNormalization(epsilon=1e-6)(x)
+    x = layers.Dropout(drop_out_rate)(x)
+    x = tf.keras.layers.Dense(ff_dim, activation='gelu')(x)
+    x = layers.Dropout(drop_out_rate)(x)
+    outputs = layers.Dense(num_classes, activation='softmax')(x)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+def seismo_transformer_hybrid(
+        maxlen=400,
+        patch_size=25,
+        patch_size_1=2,
+        patch_size_2=3,
+        num_channels=3,
+        num_patches_spec = 40,
+        nfft=128,
+        d_model=96,
+        num_heads=8,
+        ff_dim_factor=4,
+        layers_depth=8,
+        num_classes=3,
+        drop_out_rate=0.1):
+    """Create classifier model using ViT approach with transformer blocks
+    :maxlen: maximum samples of waveforms
+    :patch_size: patch size for every single channel
+    :num_channels: number of channels (usually it's equal to 3)
+    :d_model: Embedding size for each token
+    :num_heads: Number of attention heads
+    :ff_dim_factor: Hidden layer size in feed forward network inside transformer
+                    ff_dim = d_model * ff_dim_factor
+    :layers_depth: The number of transformer blocks
+    :num_classes: The number of classes to predict
+    :returns: Keras model object
+    """
+    num_patches = maxlen // patch_size
+    ff_dim = d_model * ff_dim_factor
+    inputs = layers.Input(shape=(maxlen, num_channels))
+    x = tf.keras.layers.Permute((2, 1))(inputs)
+    # patch the input channel
+    x = tf.keras.layers.Reshape((num_channels, num_patches, patch_size))(x)
+    x = RearrangeCh()(x)
+    # embedding
+    x = tf.keras.layers.Dense(d_model)(x)
+    # do transform
+    num_patches_spec = num_patches_spec
+    spec = STFT(n_fft=nfft,
+            window_name=None,
+            pad_end=False,
+            input_data_format='channels_last',
+            output_data_format='channels_last',)(inputs)
+    spec = Magnitude()(spec)
+    spec = MagnitudeToDecibel()(spec)
+    # patch the input channel
+    # x = tf.keras.layers.Reshape((num_channels, num_patches, patch_size))(x)
+    spec = Rearrange3d(p1=patch_size_1,p2=patch_size_2)(spec)
+    # embedding
+    spec = tf.keras.layers.Dense(d_model)(spec)
+    # concatenate spec and patched raw input
+    x = tf.keras.layers.Concatenate(axis=1)([x, spec])
+    # cls token
+    x = ClsToken(d_model)(x)
+    # positional embeddings
+    x = PosEmbeding(num_patches=num_patches+num_patches_spec + 1, embed_dim=d_model)(x)
+    # encoder block
+    x = layers.Dropout(drop_out_rate)(x)
+    for i in range(layers_depth):
+        x = TransformerBlock(d_model, num_heads, ff_dim)(x)
+    # to MLP head
+    x = tf.keras.layers.Lambda(lambda x: x[:, 0])(x)
+    # MLP-head
+    #x = tf.keras.layers.LayerNormalization(epsilon=1e-6)(x)
+    x = layers.Dropout(drop_out_rate)(x)
+    x = tf.keras.layers.Dense(ff_dim, activation='gelu')(x)
+    x = layers.Dropout(drop_out_rate)(x)
+    outputs = layers.Dense(num_classes, activation='softmax')(x)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
--- train.py
+++ train.py
@@ -8,7 +8,7 @@ Description: train examples on large dataset
 
 import tensorflow as tf
 from tensorflow import keras
-from seismo_transformer import load_test_train_data, seismo_transformer
+from seismo_transformer import load_test_train_data, seismo_transformer, seismo_transformer_with_spec, seismo_transformer_hybrid, seismo_performer_with_spec
 
 # Load CalTech data to NUMPY array.
 # takes 21GB of memory!!!
@@ -24,7 +24,7 @@ model = seismo_transformer(
     maxlen=400,
     patch_size=25,
     num_channels=3,
-    d_model=64,
+    d_model=32,
     num_heads=8,
     ff_dim_factor=4,
     layers_depth=8,
@@ -45,3 +45,85 @@ history = model.fit(
 )
 
 model.save_weights('WEIGHTS/model.240K.V2.hd5', save_format='h5')
+
+
+model_with_spec = seismo_transformer_with_spec(
+    maxlen=400,
+    nfft=128,
+    patch_size_1=9,
+    patch_size_2=5,
+    num_channels=3,
+    num_patches=13,
+    d_model=32,
+    num_heads=8,
+    ff_dim_factor=4,
+    layers_depth=8,
+    num_classes=3,
+    drop_out_rate=0.1)
+
+LR = 0.001
+model_with_spec.compile(
+    optimizer=keras.optimizers.Adam(learning_rate=LR),
+    loss=keras.losses.SparseCategoricalCrossentropy(),
+    metrics=[keras.metrics.SparseCategoricalAccuracy()],)
+
+print("Fit model on training data")
+history = model.fit(
+    X_train,
+    epochs=20,
+    validation_data=(X_test),
+)
+
+
+model_with_spec = seismo_performer_with_spec(
+    maxlen=400,
+    nfft=128,
+    patch_size_1=9,
+    patch_size_2=5,
+    num_channels=3,
+    num_patches=13,
+    d_model=48,
+    num_heads=8,
+    ff_dim_factor=4,
+    layers_depth=8,
+    num_classes=3,
+    drop_out_rate=0.1)
+
+LR = 0.001
+model_with_spec.compile(
+    optimizer=keras.optimizers.Adam(learning_rate=LR),
+    loss=keras.losses.SparseCategoricalCrossentropy(),
+    metrics=[keras.metrics.SparseCategoricalAccuracy()],)
+
+
+# load Dagestan test data M1.0+ fixed channel order
+X_train, X_test, y_train, y_test =\
+    load_test_train_data('/Volumes/ML_SEISMO_D/ML_DATASETS_SAKH_DAG/2020-09-10-DAGESTAN_1_FIXED.hdf5', 0.3)
+
+# load Sakhalin test data M1.0+ fixed channel order
+X_train, X_test, y_train, y_test =\
+    load_test_train_data('/Volumes/ML_SEISMO_D/ML_DATASETS_SAKH_DAG/2020-09-10-SAKHALIN_1_FIXED.hdf5', 0.3)
+
+
+print("Fit model on training data")
+history = model_with_spec.fit(
+    X_train,
+    y_train,
+    epochs=15,
+    batch_size=128,
+    validation_data=(X_test, y_test))
+
+model_hybrid = seismo_transformer_hybrid(
+        maxlen=400,
+        patch_size=25,
+        patch_size_1=9,
+        patch_size_2=5,
+        num_channels=3,
+        num_patches_spec = 13,
+        nfft=128,
+        d_model=96,
+        num_heads=8,
+        ff_dim_factor=4,
+        layers_depth=8,
+        num_classes=3,
+        drop_out_rate=0.1)
