9480382677d41299ab6412f7090338ccfd35240c
Vladimir <syler1983.9@gmail.com>
2021-08-02 17:16:47 +1000

    Up default batch-size and trace-size values

--- archive_scan.py
+++ archive_scan.py
@@ -29,9 +29,9 @@ if __name__ == '__main__':
     parser.add_argument('--out', '-o', help = 'Path to output file with predictions', default = 'predictions.txt')
     parser.add_argument('--threshold', help = 'Positive prediction threshold, default: 0.95', default = 0.95)
     parser.add_argument('--batch-size', help = 'Model batch size, default: 64 slices (4 seconds, 3 channels)',
-                        default = 64)
+                        default = 128)
     parser.add_argument('--trace-size', '-b', help = 'Length of loaded and processed seismic data stream, '
-                                                     'default: 50 000 samples', default = 50_000)
+                                                     'default: 50 000 samples', default = 100_000)
     parser.add_argument('--shift', help = 'Sliding windows shift, default: 40 samples (40 ms)', default = 40)
     parser.add_argument('--no-filter', help = 'Do not filter input waveforms', action = 'store_true')
     parser.add_argument('--no-detrend', help = 'Do not detrend input waveforms', action = 'store_true')
